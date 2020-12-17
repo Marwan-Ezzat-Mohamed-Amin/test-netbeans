ce5e3663a48f62971e66669974e9eb4181973dc1
Marwan-Ezzat-Mohamed <Marwan-Ezzat-Mohamed@users.noreply.github.com>
2020-12-18 00:21:06 +0200

    hello

--- test netbeans/src/test/netbeans/TestNetbeans.java
+++ test netbeans/src/test/netbeans/TestNetbeans.java
@@ -17,6 +17,7 @@ public class TestNetbeans {
     public static void main(String[] args) {
         // TODO code application logic here
         //removed code
+        System.out.println("fsd");
 
     }
     
